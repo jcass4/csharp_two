8a21e6fe856bdf607a8ceadd6d9678e9b66c7d4c
jcass4 <jcass4@u.brockport.edu>
2015-04-26 17:54:15 -0400

    Final User Data Form

--- BicycleRentalWPF/NewUserForm.Designer.cs
+++ BicycleRentalWPF/NewUserForm.Designer.cs
@@ -28,19 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.EnterNewUserDataLabel = new System.Windows.Forms.Label();
+            this.BannerIDLabel = new System.Windows.Forms.Label();
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.PhoneNumberLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.UserTypeLabel = new System.Windows.Forms.Label();
+            this.NotesLabel = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.DateStatusUpdatedLabel = new System.Windows.Forms.Label();
+            this.BannerIDTextBox = new System.Windows.Forms.TextBox();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.UserTypeTextBox = new System.Windows.Forms.TextBox();
+            this.NotesTextBox = new System.Windows.Forms.TextBox();
+            this.StatusTextBox = new System.Windows.Forms.TextBox();
+            this.DateStatusUpdatedTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // EnterNewUserDataLabel
+            // 
+            this.EnterNewUserDataLabel.AutoSize = true;
+            this.EnterNewUserDataLabel.Location = new System.Drawing.Point(178, 21);
+            this.EnterNewUserDataLabel.Name = "EnterNewUserDataLabel";
+            this.EnterNewUserDataLabel.Size = new System.Drawing.Size(108, 13);
+            this.EnterNewUserDataLabel.TabIndex = 0;
+            this.EnterNewUserDataLabel.Text = "Enter New User Data";
+            // 
+            // BannerIDLabel
+            // 
+            this.BannerIDLabel.AutoSize = true;
+            this.BannerIDLabel.Location = new System.Drawing.Point(98, 56);
+            this.BannerIDLabel.Name = "BannerIDLabel";
+            this.BannerIDLabel.Size = new System.Drawing.Size(61, 13);
+            this.BannerIDLabel.TabIndex = 1;
+            this.BannerIDLabel.Text = "Banner ID: ";
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.Location = new System.Drawing.Point(98, 82);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.FirstNameLabel.TabIndex = 2;
+            this.FirstNameLabel.Text = "First Name: ";
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.Location = new System.Drawing.Point(98, 107);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(64, 13);
+            this.LastNameLabel.TabIndex = 3;
+            this.LastNameLabel.Text = "Last Name: ";
+            // 
+            // PhoneNumberLabel
+            // 
+            this.PhoneNumberLabel.AutoSize = true;
+            this.PhoneNumberLabel.Location = new System.Drawing.Point(98, 136);
+            this.PhoneNumberLabel.Name = "PhoneNumberLabel";
+            this.PhoneNumberLabel.Size = new System.Drawing.Size(84, 13);
+            this.PhoneNumberLabel.TabIndex = 4;
+            this.PhoneNumberLabel.Text = "Phone Number: ";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(98, 165);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(38, 13);
+            this.EmailLabel.TabIndex = 5;
+            this.EmailLabel.Text = "Email: ";
+            // 
+            // UserTypeLabel
+            // 
+            this.UserTypeLabel.AutoSize = true;
+            this.UserTypeLabel.Location = new System.Drawing.Point(98, 191);
+            this.UserTypeLabel.Name = "UserTypeLabel";
+            this.UserTypeLabel.Size = new System.Drawing.Size(62, 13);
+            this.UserTypeLabel.TabIndex = 6;
+            this.UserTypeLabel.Text = "User Type: ";
+            // 
+            // NotesLabel
+            // 
+            this.NotesLabel.AutoSize = true;
+            this.NotesLabel.Location = new System.Drawing.Point(98, 218);
+            this.NotesLabel.Name = "NotesLabel";
+            this.NotesLabel.Size = new System.Drawing.Size(41, 13);
+            this.NotesLabel.TabIndex = 7;
+            this.NotesLabel.Text = "Notes: ";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(98, 247);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(43, 13);
+            this.StatusLabel.TabIndex = 8;
+            this.StatusLabel.Text = "Status: ";
+            // 
+            // DateStatusUpdatedLabel
+            // 
+            this.DateStatusUpdatedLabel.AutoSize = true;
+            this.DateStatusUpdatedLabel.Location = new System.Drawing.Point(98, 274);
+            this.DateStatusUpdatedLabel.Name = "DateStatusUpdatedLabel";
+            this.DateStatusUpdatedLabel.Size = new System.Drawing.Size(113, 13);
+            this.DateStatusUpdatedLabel.TabIndex = 9;
+            this.DateStatusUpdatedLabel.Text = "Date Status Updated: ";
+            // 
+            // BannerIDTextBox
+            // 
+            this.BannerIDTextBox.Location = new System.Drawing.Point(279, 53);
+            this.BannerIDTextBox.Name = "BannerIDTextBox";
+            this.BannerIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BannerIDTextBox.TabIndex = 10;
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(279, 79);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FirstNameTextBox.TabIndex = 11;
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(279, 162);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EmailTextBox.TabIndex = 12;
+            // 
+            // UserTypeTextBox
+            // 
+            this.UserTypeTextBox.Location = new System.Drawing.Point(279, 188);
+            this.UserTypeTextBox.Name = "UserTypeTextBox";
+            this.UserTypeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UserTypeTextBox.TabIndex = 13;
+            // 
+            // NotesTextBox
+            // 
+            this.NotesTextBox.Location = new System.Drawing.Point(279, 215);
+            this.NotesTextBox.Name = "NotesTextBox";
+            this.NotesTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NotesTextBox.TabIndex = 14;
+            // 
+            // StatusTextBox
+            // 
+            this.StatusTextBox.Location = new System.Drawing.Point(279, 244);
+            this.StatusTextBox.Name = "StatusTextBox";
+            this.StatusTextBox.Size = new System.Drawing.Size(100, 20);
+            this.StatusTextBox.TabIndex = 15;
+            // 
+            // DateStatusUpdatedTextBox
+            // 
+            this.DateStatusUpdatedTextBox.Location = new System.Drawing.Point(279, 271);
+            this.DateStatusUpdatedTextBox.Name = "DateStatusUpdatedTextBox";
+            this.DateStatusUpdatedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DateStatusUpdatedTextBox.TabIndex = 16;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(279, 133);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PhoneNumberTextBox.TabIndex = 17;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(279, 104);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.LastNameTextBox.TabIndex = 18;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(112, 314);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(99, 25);
+            this.SubmitButton.TabIndex = 19;
+            this.SubmitButton.Text = "SUBMIT";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(279, 314);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(99, 25);
+            this.BackButton.TabIndex = 20;
+            this.BackButton.Text = "BACK";
+            this.BackButton.UseVisualStyleBackColor = true;
             // 
             // NewUserForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 393);
+            this.ClientSize = new System.Drawing.Size(473, 351);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.DateStatusUpdatedTextBox);
+            this.Controls.Add(this.StatusTextBox);
+            this.Controls.Add(this.NotesTextBox);
+            this.Controls.Add(this.UserTypeTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.BannerIDTextBox);
+            this.Controls.Add(this.DateStatusUpdatedLabel);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.NotesLabel);
+            this.Controls.Add(this.UserTypeLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.PhoneNumberLabel);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Controls.Add(this.BannerIDLabel);
+            this.Controls.Add(this.EnterNewUserDataLabel);
             this.Name = "NewUserForm";
-            this.Text = "NewUserForm";
+            this.Text = "New User Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label EnterNewUserDataLabel;
+        private System.Windows.Forms.Label BannerIDLabel;
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.Label PhoneNumberLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label UserTypeLabel;
+        private System.Windows.Forms.Label NotesLabel;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label DateStatusUpdatedLabel;
+        private System.Windows.Forms.TextBox BannerIDTextBox;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox UserTypeTextBox;
+        private System.Windows.Forms.TextBox NotesTextBox;
+        private System.Windows.Forms.TextBox StatusTextBox;
+        private System.Windows.Forms.TextBox DateStatusUpdatedTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Button BackButton;
     }
 }
